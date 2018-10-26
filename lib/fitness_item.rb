660204bc4e37bd5b34679f1fc49ea591d02f95aa
Samuel Cornet <samuel@noukod.com>
2018-10-26 20:33:14 +0000

    sec commit

--- lib/fitness_item.rb
+++ lib/fitness_item.rb
@@ -1,4 +1,11 @@
-require "fitness_item/version"
+require "open-uri"
+require "pry "
+require "Nokogiri"
+
+require_relative "./fitness_item/version"
+require_relative ".cli.rb"
+require_relative "scraper.rb"
+
 
 module FitnessItem
   # Your code goes here...
